6037676f867395a3c39b141ab73d2ca200aeea00
anju0210 <s2309@e-mirim.hs.kr>
2024-07-22 00:14:15 +0900

    [1.7] Nicknames part One

--- src/server.js
+++ src/server.js
@@ -19,13 +19,20 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server })
 
+function onSocketClose(){
+    console.log("Disconnected from Browser");
+}
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from Browser"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf8'))
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) =>{
+        sockets.forEach((aSocket) => aSocket.send(message.toString("utf-8")));
     });
-    socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
+
