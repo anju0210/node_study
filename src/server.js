7db195b55f875754a09a6936192bcec570ab1640
anju0210 <s2309@e-mirim.hs.kr>
2024-07-22 16:42:43 +0900

    [2.8] Room Count part One

--- src/server.js
+++ src/server.js
@@ -18,6 +18,21 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms(){
+    const {
+        sockets: {
+            adapter: {sids, rooms},
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny((event)=>{
